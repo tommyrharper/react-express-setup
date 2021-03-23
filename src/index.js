64e34efe1e31102080d0a52910e455429baa1fd5
tommyrharper <thomasrobertharper@gmail.com>
2021-03-23 19:52:47 +0000

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 const Index = () => {
-  return <div>Welcome to React App!</div>;
-}
-
+    return <div>WELCOME TO REACT APP!</div>;
+};
 ReactDOM.render(<Index />, document.getElementById('app'));
